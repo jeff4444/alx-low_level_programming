a685ecbe4b475027ef2f1b6524f095168dc92201
Jeff <allojeff444@gmail.com>
2023-03-22 18:21:04 -0700

    isalpha func

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isalpha - checks is input is a letter of the aplhabet
- * @c - character checked
+ * @c: character checked
  * Return: 1 if true, 0 otherwise
  */
 int _isalpha(int c)
