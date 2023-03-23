2dc3349aab9cfc8187e9046709b29a87eec7c416
Jeff <allojeff444@gmail.com>
2023-03-22 18:49:34 -0700

    last digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,15 @@ int print_last_digit(int n)
 
 	if (n < 0)
 	{
-		n = - n;
+		n = -n;
 	}
 
 	a = (n % 10);
-
+	
+	if (a < 0)
+        {
+                a = -a;
+        }
 	_putchar(a + '0');
 	return (a);
 }
