97bf41229816dd74803ae1adf07006c67c66fbef
Jeff <allojeff444@gmail.com>
2023-04-13 16:53:38 -0700

    Initial commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,10 +4,12 @@
  * main - used to get the name of the executable file
  * @argc: length of argv
  * @argv: array of command line arguments
+ *
  * Return: 0 (Always)
  */
-int main(int argc, char* argv[])
+int main(int argc__attribute__((unused)), char* argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
+
 	return (0);
 }
