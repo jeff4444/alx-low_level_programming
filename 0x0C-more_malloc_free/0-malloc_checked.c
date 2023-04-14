221eef2d3ab37134fabf17f5e5c781c1a9ef46aa
Jeff <allojeff444@gmail.com>
2023-04-14 11:48:56 -0700

    Initial commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory
+ * @b: memory size
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+	s = malloc(b);
+	if (s == NULL)
+	{
+		exit(98);
+	}
+	return (s);
+}
