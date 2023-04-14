9483e9efd642858da010e708213807806f3bc3da
Jeff <allojeff444@gmail.com>
2023-04-14 12:13:39 -0700

    Concatenate strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,12 +15,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 
 	len1 = 0;
-	i = 0;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
 	
 	while (s1[len1] != '\0')
 	{
