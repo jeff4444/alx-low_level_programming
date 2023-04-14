cd947d0131280c5f0f28b0162616d424d6d33781
Jeff <allojeff444@gmail.com>
2023-04-14 10:51:06 -0700

    Concatenate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,8 +15,10 @@ char *str_concat(char *s1, char *s2)
 	
 	len = 0;
 	i = 0;
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
+	{
 		return (NULL);
+	}
 
 	while (s1[len] != '\0')
 	{
