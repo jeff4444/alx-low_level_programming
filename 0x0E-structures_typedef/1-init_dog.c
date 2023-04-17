37f6401bb32c5bf5b9d35ad08ca04c7b15846e99
Jeff <allojeff444@gmail.com>
2023-04-17 11:37:47 -0700

    print an init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog - initializes a variable of type struct dog
  * @d: dog struct
@@ -8,7 +9,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,10 +12,7 @@ void print_dog(struct dog *d)
 	else
 		printf("%s\n", d->name);
 	printf("Age: ");
-	if (d->age == NULL)
-		printf("(nil)\n");
-	else
-		printf("%f\n", d->age);
+	printf("%f\n", d->age);
 	printf("Owner: ");
 	if (d->owner == NULL)
 		printf("(nil)\n");
