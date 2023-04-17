19cf0219d4edccde3eb2c23c42a27ead9676f995
Jeff <allojeff444@gmail.com>
2023-04-17 11:00:38 -0700

    Initial commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef dog_h
 #define dog_h
+/**
+ * struct dog - creates a dog struct with name
+ * age and owner members
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ */
 struct dog
 {
 	char *name;
