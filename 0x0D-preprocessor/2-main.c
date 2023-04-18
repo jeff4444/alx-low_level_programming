d8839321c1a500cf3bd47dca49e3f7239533a89e
Jeff <allojeff444@gmail.com>
2023-04-17 18:24:22 -0700

    Edited main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,10 +2,12 @@
 /**
  * main - prints current file name
  *
- * Return: 0 (Success)
+ * Return: 0 (Success), 1 (FAIL)
  */
 int main()
 {
+	if (__FILE__ == NULL)
+		return (1);
 	printf("%s\n", __FILE__);
 	return (0);
 }
