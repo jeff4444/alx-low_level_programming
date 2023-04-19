9d73fe4d4791e5a7d5804bb77b6656e3e10e1cd1
Jeff <allojeff444@gmail.com>
2023-04-19 10:25:27 -0700

    Iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef function_h
 #define function_h
+#include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
