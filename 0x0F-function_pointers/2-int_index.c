e900baf26d74d2d8db71fdb141a6e9e767193a97
Jeff <allojeff444@gmail.com>
2023-04-19 10:41:59 -0700

    Search for int

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,16 +5,16 @@
  * @array: list of integers
  * @size: size of array
  * @cmp: function to decide integer
- * 
+ *
  * Return: index of integer or -1 if size <= 0 or no matches
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size < 1)
+	if (size <= 0)
 		return (-1);
-	
+
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
