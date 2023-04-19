0a62498e826d4b3d9b1b6c0c9ff636bd2ddc6355
Jeff <allojeff444@gmail.com>
2023-04-19 10:22:10 -0700

    Iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL)
 	{
-		array = malloc(sizeof(int) * size);
+		array = (int*) malloc(sizeof(int) * size);
 	}
 
 	for (i = 0; i < size; i++)
