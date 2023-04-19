a6cef06007215e50cb24cd7f299e57157698cccf
Jeff <allojeff444@gmail.com>
2023-04-19 10:43:40 -0700

    Removed set memory allocation

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,11 +13,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
-	if (array == NULL)
-	{
-		array = (int*) malloc(sizeof(int) * size);
-	}
-
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
