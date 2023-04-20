32990ed8ab7a8ee4ee8a9d6d1abb18eb659a8f28
Jeff <allojeff444@gmail.com>
2023-04-20 06:57:46 -0700

    Edited to match betty style

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,13 +9,16 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	int i, sum;
+
 	va_list args;
 
 	va_start(args, n);
+
 	sum = 0;
 
 	if (n == 0)
 		return (0);
+
 	for (i = 0; i < (int) n; i++)
 	{
 		sum += va_arg(args, int);
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,5 +20,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (separator != NULL && i != ((int) n) - 1)
 			printf("%s", separator);
 	}
+	va_end(args);
 	printf("\n");
 }
