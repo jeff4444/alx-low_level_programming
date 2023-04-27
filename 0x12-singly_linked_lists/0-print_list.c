cdb977436c77689b34a30a6081eb885cb0dc7561
Jeff Mofor <allojeff444@gmail.com>
2023-04-27 12:25:42 -0700

    Printing linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_list - prints all elements in a linked list
+ * @h: linked list
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+	list_t *head = h;
+
+	while (head != NULL)
+	{
+		if (head->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", head->len, head->str);
+		head = head->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef LIST_H
+#define LIST_H
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * struct node - linked list node
+ * @str: string value
+ * @len: length of string
+ * @next: next node
+ */
+struct node
+{
+	char *str;
+	int len;
+	struct node *next;
+};
+
+typedef struct node list_t;
+size_t print_list(const list_t *h);
+
+#endif
