cea4b5f72c4d991bb9dbeec147292ec737a08c68
Jeff Mofor <allojeff444@gmail.com>
2023-04-27 12:53:27 -0700

    Length of list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * list_len - returns number of elements in a linked
+ * list
+ * @h: linked list
+ *
+ * Return: number of elements in h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+		i++;
+	return (i);
+}
