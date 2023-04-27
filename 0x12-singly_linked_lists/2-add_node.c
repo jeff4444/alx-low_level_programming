ec914887a2ba9f4e29dbd1450e099eba1a87e338
Jeff Mofor <allojeff444@gmail.com>
2023-04-27 13:43:44 -0700

    Fixed

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -25,5 +25,6 @@ list_t *add_node(list_t **head, const char *str)
 	nextNode->str = newStr;
 	nextNode->len = len;
 	nextNode->next = (*head);
-	return (nextNode);
+	*head = nextNode;
+	return (*head);
 }
