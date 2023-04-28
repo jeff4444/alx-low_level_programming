ed316903e05ff8a08c7193e84aa1cb0be69a86e5
Jeff Mofor <allojeff444@gmail.com>
2023-04-27 18:55:29 -0700

    Print first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints a string
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
