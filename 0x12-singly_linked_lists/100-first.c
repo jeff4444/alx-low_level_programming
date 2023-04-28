d7a1c1cba23f57438a79b88d76a6877a8a6d6983
Jeff Mofor <allojeff444@gmail.com>
2023-04-27 19:23:53 -0700

    Initial assembly code commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void before_main() __attribute__((constructor));
 /**
  * before_main - prints a string
  *
  * Return: none
  */
+void before_main() __attribute__((constructor));
 void before_main()
 {
 	printf("You're beat! and yet, you must allow,\n");
