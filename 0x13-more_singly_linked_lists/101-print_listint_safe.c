89640774b8e13361ba30c5303bf1a284fae35c78
Jeff Mofor <allojeff444@gmail.com>
2023-05-02 16:55:34 -0700

    Printing safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,10 +8,12 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	int i = 0;
+
 	while (head)
 	{
-		printf("[%p] %d\n", head, head->n);
+		printf("[%p] %d\n", (void *)head, head->n);
 		i++;
+		head = head->next;
 	}
 	return (i);
 }
