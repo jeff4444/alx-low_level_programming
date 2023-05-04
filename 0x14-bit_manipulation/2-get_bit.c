aa90ed3a9df5b247c9d64d186c5198eaff6f68d2
Jeff Mofor <allojeff444@gmail.com>
2023-05-04 16:25:48 -0700

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * get_bit - returns the value of a bit at an idx
+ * @n: number to use
+ * @index: index
+ *
+ * Return: bit at index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int value = 1;
+
+	while (index--)
+	{
+		value *= 2;
+	}
+	if (value > n)
+		return (-1);
+	if (n & value)
+		return (1);
+	return (0);
+}
