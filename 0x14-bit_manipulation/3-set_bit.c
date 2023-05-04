f90214e89d063497f018cd6807eed397d60a6ba9
Jeff Mofor <allojeff444@gmail.com>
2023-05-04 16:38:00 -0700

    Setting bits

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * set_bit - sets a value of a bit to 1
+ * @n: number
+ * @index: given index
+ *
+ * Return: 1 (sucess) 0 (Fail)
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int text = 1;
+
+	while (index--)
+		text *= 2;
+	*n = *n | text;
+	return (1);
+}
