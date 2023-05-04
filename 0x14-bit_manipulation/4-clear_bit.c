f5b540781ebb5248fdf69af17529b1e1c83f4ff8
Jeff Mofor <allojeff444@gmail.com>
2023-05-04 16:55:24 -0700

    Clearing bits

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * clear_bit - sets a value of a bit to 0
+ * @n: number
+ * @index: given index
+ *
+ * Return: 1 (sucess) -1 (Fail)
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int text = 1;
+
+	while (index--)
+	{
+		text *= 2;
+	}
+	*n = *n & (~ text);
+	return (1);
+}
