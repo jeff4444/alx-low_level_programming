c7df1b233dc32c1c0f8b2931ca148ae5a653a3bf
Jeff Mofor <allojeff444@gmail.com>
2023-05-08 20:00:01 -0700

    Task 2

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: filename
+ * @text_content: text content
+ * Return: 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+
