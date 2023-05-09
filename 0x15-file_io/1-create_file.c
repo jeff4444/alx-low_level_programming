3f235c5691ae1e3e0b5eda125802d6d2374b2290
Jeff Mofor <allojeff444@gmail.com>
2023-05-09 11:12:56 -0700

    Fixed buffer error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		close(file);
 		return (0);
 	}
-	len = write(file, text_content, strlen(buffer));
+	len = write(file, text_content, strlen(text_content));
 	if (len == -1)
 		return (-1);
 	close(file);
