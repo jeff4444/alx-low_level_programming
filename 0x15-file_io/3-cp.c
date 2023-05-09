f34bc11aa22c6779cfb814c5c15eafad4ec0ce50
Jeff Mofor <allojeff444@gmail.com>
2023-05-09 15:33:34 -0700

    Fixed numeratiosn

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,21 +12,21 @@ int main(int argc, char *argv[])
 	ssize_t len1, len2;
 	char *buffer;
 
-	if (argc != 2)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to %s\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_from = open(argv[0], O_RDONLY);
+	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[0]);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
 		exit(98);
 	}
-	fd_to = creat(argv[1], S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fd_to = creat(argv[2], S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	buffer = malloc(1024);
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 
         if (len1 == -1)
         {
-                dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[0]);
+                dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
                 exit(98);
         }
 	while (len1 == 1024)
@@ -48,20 +48,20 @@ int main(int argc, char *argv[])
 		len2 = write(fd_to, buffer, 1024);
 		if (len2 == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 		len1 = read(fd_from, buffer, 1024);
 		if (len1 == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[0]);
+			dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
 			exit(98);
 		}
 	}
 	len2 = write(fd_to, buffer, strlen(buffer));
 	if (len2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	cl_to = close(fd_to);
