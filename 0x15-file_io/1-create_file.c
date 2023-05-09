c89d90f30119997a4cee015f8e36a363f7441aa7
Jeff Mofor <allojeff444@gmail.com>
2023-05-09 12:52:40 -0700

    Fixed bug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,10 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 	if (text_content == NULL)
-	{
-		close(file);
-		return (0);
-	}
+		text_content = "";
 	len = write(file, text_content, strlen(text_content));
 	if (len == -1)
 		return (-1);
