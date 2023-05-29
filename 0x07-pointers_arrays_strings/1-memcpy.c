296c8042891a4dcd1470e811f824bc63c0cbd235
Jeff Mofor <allojeff444@gmail.com>
2023-05-29 13:12:22 -0700

    More bugs

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,7 +11,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i = 0;
 
 	while (i < n && dest[i] != '\0' && src[i] != '\0')
-		dest[i] = src[i++];
+	{
+		dest[i] = src[i];
+		i++;
+	}
 	if (src[i] == '\0')
 		dest[i] = '\0';
 	return (dest);
