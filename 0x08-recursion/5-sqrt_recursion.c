9a973caafdfda370c8b90fb07bb8939df4e42270
Jeff Mofor <allojeff444@gmail.com>
2023-05-28 18:41:34 -0700

    Recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - calculates sqrt
+ * @n: number
+ * Return: natural sqrt
+ */
+int _sqrt_recursion(int n)
+{
+	(void)n;
+	return (1);
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * is_prime_number - is a prime number or not
+ * @n: number
+ * Return: 1 True, 0 False
+ */
+int is_prime_number(int n)
+{
+	(void)n;
+	return (0);
+}
