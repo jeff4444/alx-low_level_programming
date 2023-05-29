5474c4e7dd33eee5029e76436ea837fcc696862b
Jeff Mofor <allojeff444@gmail.com>
2023-05-29 13:09:42 -0700

    Fixed bugs

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef STRING_H
 #define STRING_H
+#include <stdlib.h>
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+void simple_print_buffer(char *buffer, unsigned int size);
 #endif
