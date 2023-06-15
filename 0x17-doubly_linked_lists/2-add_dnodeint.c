48d17de02383ef0bf13edf7c7df1a90f618c0ec2
Jeff Mofor <allojeff444@gmail.com>
2023-06-15 06:44:29 -0700

    Fixed prev pointer in 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,6 +16,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newNode->n = n;
 	newNode->prev = NULL;
 	newNode->next = *head;
+	if (*head)
+		(*head)->prev = newNode;
 	*head = newNode;
 	return (newNode);
 }
