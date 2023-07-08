3b18a573f29de9b01463c05c20e6964335953663
Jeff Mofor <allojeff444@gmail.com>
2023-07-07 19:06:16 -0700

    used modulo

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,7 +10,5 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index = hash_djb2(key);
 
-	if (index < size)
-		return (index);
-	return (index);
+	return (index % size);
 }
