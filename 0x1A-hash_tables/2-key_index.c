d94f301c4cd77fe476aefe555dbfd1cedc9daa19
Jeff Mofor <allojeff444@gmail.com>
2023-07-07 18:34:54 -0700

    Get index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - return the index of the key
+ * @key: key
+ * @size: size of the array of the hash table
+ * Return: index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index = hash_djb2(key);
+
+	if (index < size)
+		return (index);
+	return (NULL);
+}
