c9453b6af609954097aea6c6fab45ef30e1cf3c3
Jeff Mofor <allojeff444@gmail.com>
2023-07-07 18:08:52 -0700

    Fixed some errors

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,6 @@
-#ifndef "HASH_H"
-#define "HASH_H"
-
+#ifndef HASH_H
+#define HASH_H
+#include "stdlib.h"
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -28,7 +28,7 @@ typedef struct hash_table_s
 {
 	unsigned long int size;
 	hash_node_t **array;
-} hash_table_t
+} hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
 #endif
