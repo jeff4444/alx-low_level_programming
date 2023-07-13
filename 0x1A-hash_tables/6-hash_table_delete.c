535adac02e847699b78b014c84054f1673033de0
Jeff Mofor <allojeff444@gmail.com>
2023-07-12 18:00:17 -0700

    Delete table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * free_node - frees node
+ * @node: node
+ */
+void free_node(hash_node_t *node)
+{
+	hash_node_t *cur;
+
+	while ((cur = node))
+	{
+		node = node->next;
+		free(cur->key);
+		free(cur->value);
+	}
+	free(node);
+}
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+
+	if (ht == NULL)
+		return;
+	for (; i < ht->size; i++)
+		free_node(ht->array[i]);
+	free(ht);
+}
