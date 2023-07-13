489fa6a63466c72646bc306012927272fbaa4fd2
Jeff Mofor <allojeff444@gmail.com>
2023-07-12 17:38:35 -0700

    Print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	int on = 0;
+	unsigned long int size = ht->size, i = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (; i < size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (on)
+				printf(", ")
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			on = 1;
+		}
+	}
+	print("}");
+}	
